586429aa134427fe80518463b38778331308dd82
kaushiktd <kaushiktd@gmail.com>
2024-09-12 16:43:50 +0530

    stack added

--- src/java_collections/list_iterface/CollectionsStack.java
+++ src/java_collections/list_iterface/CollectionsStack.java
@@ -1,5 +1,20 @@
 package java_collections.list_iterface;
 
+import java.util.Stack;
+
 public class CollectionsStack {
 
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Aluminium");
+        stack.push("Galium");
+        stack.push("Indium");
+        stack.push("Tin");
+        stack.push("Thalium");
+        stack.push("lead");
+
+        System.out.println(stack.peek());
+
+    }
 }
